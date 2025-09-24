4bb55c397ace4d85206ffaf6b20a218e805e11ad
Vidhya Sathya Murthy <vidhyasathyamurthy04@gmail.com>
2025-09-24 17:26:29 +0000

    i want the logo to open and close the eye

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -14,7 +14,21 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       className={cn("text-primary", props.className)}
       {...props}
     >
-      <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
+      <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z">
+         <animate
+          attributeName="d"
+          dur="3s"
+          repeatCount="indefinite"
+          keyTimes="0; 0.4; 0.5; 0.6; 1"
+          values="
+            M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z;
+            M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z;
+            M2 12s3-1 10-1 10 1 10 1-3 1-10 1-10-1-10-1Z;
+            M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z;
+            M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z
+          "
+        />
+      </path>
       <circle cx="12" cy="12" r="3" />
     </svg>
   );
